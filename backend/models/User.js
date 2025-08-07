31373d08e4e11660ba67eb612a67f4f2b7b453d8
HUHGEON <rjs2337@naver.com>
2025-08-07 09:24:27 +0900

    [SERV]#7 : 오류 메시지 변경

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,43 +4,42 @@ import bcrypt from 'bcryptjs';
 const userSchema = new mongoose.Schema({
   id: {
     type: String,
-    required: [true, '사용자 아이디는 필수입니다'],
+    required: [true, '아이디를 입력해 주세요'],
     unique: true,
     trim: true,
-    minlength: [3, '아이디는 3글자 이상이어야 합니다'],
-    maxlength: [10, '아이디는 10글자 이하여야 합니다']
+    minlength: [3, '아이디는 3글자 이상으로 생성해 주세요'],
+    maxlength: [10, '아이디는 10글자 이하로 생성해 주세요']
   },
   pw: {
     type: String,
     required: [true, '비밀번호는 필수입니다'],
-    minlength: [8, '비밀번호는 8글자 이상이어야 합니다'],
-    maxlength: [15, '비밀번호는 15글자 이하여야 합니다'],
+    minlength: [8, '비밀번호는 8글자 이상으로 생성해 주세요'],
+    maxlength: [15, '비밀번호는 15글자 이하로 생성해 주세요'],
     validate: {
       validator: function(password) {
         // 문자와 숫자만 허용, 공백 없음
         const passwordRegex = /^[a-zA-Z0-9]{8,16}$/;
         return passwordRegex.test(password);
       },
-      message: '비밀번호는 8-16자리이며, 영문자와 숫자만 사용 가능합니다(공백 불가)'
+      message: '비밀번호는 8-15자리이며, 영문자와 숫자만 사용 가능합니다'
     }
   },
   name: {
     type: String,
-    required: [true, '이름은 필수입니다'],
+    required: [true, '이름은 입력해 주세요'],
     trim: true,
-    maxlength: [5, '이름은 5글자 이하여야 합니다']
   },
   nickname: {
     type: String,
     required: [true, '닉네임은 필수입니다'],
     unique: true,
     trim: true,
-    minlength: [2, '닉네임은 2글자 이상이어야 합니다'],
-    maxlength: [10, '닉네임은 15글자 이하여야 합니다']
+    minlength: [2, '닉네임은 2글자 이상으로 생성해 주세요'],
+    maxlength: [10, '닉네임은 10글자 이하로 생성해 주세요']
   },
   birth_date: {
     type: Date,
-    required: [true, '생년월일은 필수입니다']
+    required: [true, '생년월일을 입력해 주세요']
   },
   followers: {
     type: [{
