cd9907f28bd45e022990eb3af020eb9d2f9a2f3e
HUHGEON <rjs2337@naver.com>
2025-08-27 16:02:23 +0900

    [SERV]#17 : 스토리관련 스키마 추가

---
+++ backend/models/Story.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const storySchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: [true, '스토리 내용을 입력해 주세요'],
+    trim: true,
+    maxlength: [20, '스토리는 30글자 이하로 입력해 주세요']
+  },
+  image_url: {
+    type: String,
+    required: [true, '스토리 이미지를 업로드해주세요']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+  viewed_by: {
+    type: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }],
+    default: []
+  }
+}, {
+  collection: 'stories'
+});
+
+storySchema.index({ createdAt: 1 }, { expireAfterSeconds: 86400 });
+
+export default mongoose.model('Story', storySchema);
